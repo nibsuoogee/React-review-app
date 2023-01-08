8db03d8c5bbd5c68a8006f9df6624770c923ce70
temevh <temehama@gmail.com>
2023-01-08 12:29:28 +0200

    added commenting for menu

--- public/karuselli.js
+++ public/karuselli.js
@@ -1,5 +1,4 @@
 const track = document.getElementById("image-track")
-
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
 }
@@ -12,19 +11,22 @@ window.onmouseup = () => {
 
 
 window.onmousemove = e => {
+    //If mouse has not been moved
     if (track.dataset.mouseDownAt === "0") return;
 
+    //The amount the mouse has moved on screen
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, 
         maxDelta = window.innerWidth / 2;
 
+    //The amount that the menu should be moved, based on the mouse movement
     const percentage = (mouseDelta / maxDelta) * -100,
         nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-
     Math.min(nextPercentage, 0);
     Math.max(nextPercentage, -100);
 
     track.dataset.percentage = nextPercentage;
 
+    //Add animation to make the movement of the menu smoother
     track.animate({
         transform: `translate(${nextPercentage}%, -50%)`
     }, {duration: 1200, fill: "forwards"});
