8c7f6afec716e9e7cb57773fa7d52affd0071ad4
temevh <temehama@gmail.com>
2023-01-09 13:21:42 +0200

    started to refactor menu to react from vanilla js

---
+++ src/components/Menu/menu.js
@@ -0,0 +1,60 @@
+import { useRef, useEffect } from 'react';
+
+function ImageTrack() {
+  const trackRef = useRef(null);
+
+  useEffect(() => {
+    const track = trackRef.current;
+
+    const handleMouseDown = (e) => {
+      track.dataset.mouseDownAt = e.clientX;
+    };
+
+    const handleMouseUp = () => {
+      track.dataset.mouseDownAt = '0';
+      track.dataset.prevPercentage = track.dataset.percentage;
+    };
+
+    const handleMouseMove = (e) => {
+      if (track.dataset.mouseDownAt === '0') return;
+
+      const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+      const maxDelta = window.innerWidth / 2;
+      const percentage = (mouseDelta / maxDelta) * -100;
+      const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+      Math.min(nextPercentage, 0);
+      Math.max(nextPercentage, -100);
+
+      track.dataset.percentage = nextPercentage;
+
+      track.animate({
+        transform: `translate(${nextPercentage}%, -50%)`,
+      }, { duration: 1200, fill: 'forwards' });
+
+      for (const image of track.getElementsByClassName('image')) {
+        image.animate({
+          objectPosition: `${100 + nextPercentage}% center`,
+        }, { duration: 1200, fill: 'forwards' });
+      }
+    };
+
+    window.addEventListener('mousedown', handleMouseDown);
+    window.addEventListener('mouseup', handleMouseUp);
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousedown', handleMouseDown);
+      window.removeEventListener('mouseup', handleMouseUp);
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div id="image-track" ref={trackRef}>
+      {/* Images go here */}
+    </div>
+  );
+}
+
+export default ImageTrack
