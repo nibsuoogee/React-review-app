536a616b69c487e10e0e9cdaf6e56f7aa77eeb8c
temevh <temehama@gmail.com>
2023-01-10 11:10:35 +0200

    started styling the form

--- src/components/AddBook/Modal.jsx
+++ src/components/AddBook/Modal.jsx
@@ -4,26 +4,25 @@ import './Modal.css'
 const Modal = ({open, onClose}) => {
     if (!open) return null;
   return (
-    <div classname="addbookpopup">
+    <div classname="container">
+        <div>
+            <form>
+                <p onClick={onClose} className='closeBtn'>X</p>
+                <label for="name">Name </label>
+                <input type="text" id="name" name="name" placeholder="Name of the book"/>
 
-    
-        <form>
-            <p onClick={onClose} className='closeBtn'>X</p>
-            <label for="name">Name </label>
-            <input type="text" id="name" name="firstname" placeholder="Name of the book"/>
+                <label for="author">Author </label>
+                <input type="text" id="author" name="author" placeholder="Author..."/>
 
-            <label for="lname">Author </label>
-            <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
-
-            <label for="country">Country</label>
-            <select id="country" name="country">
-                <option value="scifi">Sci-fi</option>
-                <option value="drama">Drama</option>
-                <option value="science">Science</option>
-            </select>
-
-            <input type="submit" value="Submit"/>
-        </form>
+                <label for="genre">Genre</label>
+                <select id="genre" name="genre">
+                    <option value="scifi">Sci-fi</option>
+                    <option value="drama">Drama</option>
+                    <option value="science">Science</option>
+                </select>
+                <input type="submit" value="Save"/>
+            </form>
+        </div>
     </div>
   )
 }
