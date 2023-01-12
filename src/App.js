20b1c7f4e702cc3505fac65ac5c3b68fcc73b1e0
temevh <temehama@gmail.com>
2023-01-12 13:46:09 +0200

    added review pop-up functionality

--- src/App.js
+++ src/App.js
@@ -30,16 +30,106 @@ function App() {
       review: "One of the books of all time, highly recommend to anyone interested in society and surveillance.",
       image: "https://kbimages1-a.akamaihd.net/a5312ed2-bc80-4f4c-972b-c24dc5990bd5/166/300/False/george-orwell-1984-4.jpg",
       stars: 4
-    },
-    {
+  },
+  {
       id: 2,
+      title: "To kill a mockingbird",
+      author: "Harper Lee",
+      genres: ["Drama"],
+      review: "Its a book",
+      image: "https://cdn.britannica.com/21/182021-050-666DB6B1/book-cover-To-Kill-a-Mockingbird-many-1961.jpg",
+      stars: 3
+  },
+  {
+      id: 3,
       title: "Metro 2033",
-      author: "	Dmitri Gluhovski",
+      author: "Dmitry Glukhovski",
       genres: ["Dystopian","Sci-fi"],
-      review: "If you like the games, you will love the books",
-      image: "https://upload.wikimedia.org/wikipedia/fi/thumb/8/8b/Metro_2033.jpg/200px-Metro_2033.jpg",
+      review: "Its a book about metros",
+      image: "https://m.media-amazon.com/images/I/41ZAxvFijWL._AC_SY1000_.jpg",
       stars: 5
-    }
+    },
+  {
+      id: 4,
+      title: "Metro 2034",
+      author: "Dmitry Glukhovski",
+      genres: ["Dystopian","Sci-fi"],
+      review: "Sequel to metro 2033",
+      image: "https://kbimages1-a.akamaihd.net/157c78fa-dc13-4786-99af-fcfc81125171/1200/1200/False/metro-2034-6.jpg",
+      stars: 5
+    },
+  {
+      id: 5,
+      title: "Metro 2035",
+      author: "Dmitry Glukhovski",
+      genres: ["Dystopian","Sci-fi"],
+      review: "Another book about metros",
+      image: "https://m.media-amazon.com/images/I/51Y-B5KVppL.jpg",
+      stars: 5
+    },
+  {
+      id: 6,
+      title: "Javascript for dummies",
+      author: "JS Cript",
+      genres: ["Horror"],
+      review: "Help D:",
+      image: "https://m.media-amazon.com/images/I/51IWL7y8ecL._AC_SY780_.jpg",
+      stars: 5
+    },
+  {
+      id: 1,
+      title: "1984",
+      author: "George Orwell",
+      genres: ["Dystopian","Sci-fi"],
+      review: "One of the books of all time, highly recommend to anyone interested in society and surveillance.",
+      image: "https://kbimages1-a.akamaihd.net/a5312ed2-bc80-4f4c-972b-c24dc5990bd5/166/300/False/george-orwell-1984-4.jpg",
+      stars: 4
+  },
+  {
+      id: 2,
+      title: "To kill a mockingbird",
+      author: "Harper Lee",
+      genres: ["Drama"],
+      review: "Its a book",
+      image: "https://cdn.britannica.com/21/182021-050-666DB6B1/book-cover-To-Kill-a-Mockingbird-many-1961.jpg",
+      stars: 3
+  },
+  {
+      id: 3,
+      title: "Metro 2033",
+      author: "Dmitry Glukhovski",
+      genres: ["Dystopian","Sci-fi"],
+      review: "Its a book about metros",
+      image: "https://m.media-amazon.com/images/I/41ZAxvFijWL._AC_SY1000_.jpg",
+      stars: 5
+    },
+  {
+      id: 4,
+      title: "Metro 2034",
+      author: "Dmitry Glukhovski",
+      genres: ["Dystopian","Sci-fi"],
+      review: "Sequel to metro 2033",
+      image: "https://kbimages1-a.akamaihd.net/157c78fa-dc13-4786-99af-fcfc81125171/1200/1200/False/metro-2034-6.jpg",
+      stars: 5
+    },
+  {
+      id: 5,
+      title: "Metro 2035",
+      author: "Dmitry Glukhovski",
+      genres: ["Dystopian","Sci-fi"],
+      review: "Another book about metros",
+      image: "https://m.media-amazon.com/images/I/51Y-B5KVppL.jpg",
+      stars: 5
+    },
+  {
+      id: 6,
+      title: "Javascript for dummies",
+      author: "JS Cript",
+      genres: ["Horror"],
+      review: "Help D:",
+      image: "https://m.media-amazon.com/images/I/51IWL7y8ecL._AC_SY780_.jpg",
+      stars: 5
+    },
   ]);
 
   const [openBookPopup, setOpenBookPopup] = useState(false) 
@@ -62,16 +152,16 @@ function App() {
   return (
     <body>
       <div className="App">
-        <button onClick={() => HandleSetCurrentBook(2)}>Open Review Popup</button>
+        {/*<button onClick={() => HandleSetCurrentBook(2)}>Open Review Popup</button>*/}
           <ReviewPopup triggerBook={currentBook} setTrigger={HandleSetCurrentBook}
            setBookReview={handleNewBookReview} mediaItems={mediaItems}>
           </ReviewPopup>
-        <button onClick={() => setOpenBookPopup(true)}>Add a book</button>
+        {/*<button onClick={() => setOpenBookPopup(true)}>Add a book</button>*/}
           <AddBook open={openBookPopup} onClose={() => setOpenBookPopup(false)} />
         <Slider {...settings}>
         {books.map((item) =>(
           <div className="card">
-            <img src={item.image} alt={item.title}/>
+            <img src={item.image} alt={item.title} onClick={() => HandleSetCurrentBook(item.id)}/>
         </div>
         ))}
         </Slider>
