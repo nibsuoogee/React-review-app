b403a2e0c183295594a18050c6fadb09fc75576e
nibsuoogee <elias.syyrila@gmail.com>
2023-01-12 10:43:40 +0200

    working currentBook displays media in ReviewPopup

--- src/components/ReviewPopup/ReviewPopup.js
+++ src/components/ReviewPopup/ReviewPopup.js
@@ -4,13 +4,12 @@ import Rate from '../StarRating/StarRating'
 
 
 function ReviewPopup(props) {
-
-    return (props.trigger) ? (
+    return (props.triggerBook) ? (
         <div className="reviewpopup">
             <div className="reviewpopup-inner">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-                <button className="close-btn" onClick={() => props.setTrigger(false)}><i class="fa fa-close"></i></button>
-                {props.mediaItems.map((media) => (
+                <button className="close-btn" onClick={() => props.setTrigger(4)}><i class="fa fa-close"></i></button>
+                {props.triggerBook.map((media) => (
                     <div className="review-box" key={media.id}>
                         <table>
                             <tr>
@@ -18,7 +17,7 @@ function ReviewPopup(props) {
                                     <img src={media.image} alt="Media cover image"/>
                                     <div className="star-rating">
                                         <Rate rating={media.stars} 
-                                        onRating={(rate) => props.setMedia({...props.mediaItems, stars: rate})}/>
+                                        onRating={(rate) => props.setBookReview(rate)}/>
                                     </div>
                                     
                                 </td>
