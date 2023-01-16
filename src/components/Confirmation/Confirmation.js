4fdec96cea29c85c777444f739af5768394fd630
nibsuoogee <elias.syyrila@gmail.com>
2023-01-16 16:03:32 +0200

    confirm popup layout

--- src/components/Confirmation/Confirmation.js
+++ src/components/Confirmation/Confirmation.js
@@ -7,13 +7,15 @@ const Confirmation = ({open, onClose, onConfirm}) => {
     return (open) ? (
         <div className="confirmpopup">
             <div className="confirmpopup-inner">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                <button className="confirm-btn" onClick={() => onConfirm()}><i class="fa fa-trash"></i></button>
+                
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
                 <button className="close-btn" onClick={() => onClose()}><i class="fa fa-close"></i></button>
 
                 <label>Are you sure you want to delete this entry?</label>
                 
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-                <button className="confirm-btn" onClick={() => onConfirm()}><i class="fa fa-trash"></i></button>
+                
             </div>
         </div>
     ) : "";
